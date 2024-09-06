3e8d1b0420312b68fa16591e5a5e24e670b7410a
BeDauPro <tbnduc2k3@gmail.com>
2024-09-06 22:38:52 +0700

    second commit

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import {fetchAllUser} from '../services/UserService'
+import ReactPaginate from 'react-paginate';
 
 const TableUsers =(props)=>{
     const [listUsers, setListUsers]= useState([]);
+    const [totalUsers, setTotalUsers] = useState(0);
+    const[totalPages, setTotalPages]= useState(0);
     useEffect(()=>
         {
-            getUsers();
+            getUsers(1);
         },[])
-        const getUsers = async() =>{
-            let res = await fetchAllUser();
-            if(res && res.data && res.data.data){
-                setListUsers(res.data.data)
+        const getUsers = async(page) =>{
+            let res = await fetchAllUser(page);
+            if(res && res.data){
+                setTotalUsers(res.total)
+                setListUsers(res.data)
+                setTotalPages(res.total_pages);
             }
         }
-        console.log(listUsers)
+        // console.log(listUsers)
+        const handlePageClick = (event) => {
+          getUsers(+event.selected +1);
+        }
     return (<>
     <Table striped bordered hover>
       <thead>
@@ -29,7 +37,7 @@ const TableUsers =(props)=>{
         {listUsers && listUsers.length > 0 &&
         listUsers.map((item, index)=>{
             return(
-                <tr key={"user - ${index}"}>
+                <tr key={`user - ${index}`}>
                 <td>{item.id}</td>
                 <td>{item.email}</td>
                 <td>{item.first_name}</td>
@@ -40,6 +48,26 @@ const TableUsers =(props)=>{
         }
       </tbody>
     </Table>
+    <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        
+        pageClassName="page-item"
+        pageLinkClassName='page-link'
+        previousClassName='page-item'
+        previousLinkClassName='page-link'
+        nextClassName='page-item'
+        nextLinkClassName='page-link'
+        breakClassName='page-item'
+        breakLinkClassName='page-link'
+        containerClassName='pagination'
+        activeClassName='active'
+      />
     
     </>)
 }
