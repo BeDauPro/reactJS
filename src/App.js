0c7905ea1e3c20b93e07a3740414627ea45114d5
BeDauPro <tbnduc2k3@gmail.com>
2024-09-16 11:14:07 +0700

    login layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TableUsers from './components/TableUsers';
 import { ToastContainer } from 'react-toastify';
 import Home from './components/Home';
 import {Routes, Route, Link} from "react-router-dom";
+import Login from './components/Login';
 
 function App() {
   
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/users' element={<TableUsers/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </Container>
    
